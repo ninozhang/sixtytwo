80246e26148bf97bb7f38cfefc11775847022d24
nino <i@zhang.wf>
2018-01-26 01:37:49 +0800

    更新

--- test/test.js
+++ test/test.js
@@ -9,6 +9,9 @@ describe('sixtytwo', function() {
       sixtytwo.toString(0).should.equal('0');
       sixtytwo.toString(201314).should.equal('Qn0');
       sixtytwo.toString(999999999999).should.equal('hBxM5A3');
+      sixtytwo.toString(1491670057145).should.equal('qgdVxI5');
+      sixtytwo.toString(2524579200000).should.equal('IrGR2GQ');
+      sixtytwo.toString(3521614606207).should.equal('ZZZZZZZ');
     });
   });
 
@@ -17,6 +20,7 @@ describe('sixtytwo', function() {
       sixtytwo.parseInt('0').should.equal(0);
       sixtytwo.parseInt('Qn0').should.equal(201314);
       sixtytwo.parseInt('hBxM5A3').should.equal(999999999999);
+      sixtytwo.parseInt('ZZZZZZZ').should.equal(3521614606207);
     });
   });
 
